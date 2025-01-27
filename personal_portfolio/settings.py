b90641e58b2be0c299ab9be0bd39b25425d58eaa
Phuoc Nguyen <phuocnguyen102800@gmail.com>
2025-01-26 16:05:27 -0800

    uhh please work

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -128,8 +128,15 @@ USE_TZ = True
 # DEFAULT_AUTO_FIELD = 'django.ls.BigAutoField'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_ROOT = BASE_DIR / "uploads/"
-MEDIA_URL = "media/"
+# MEDIA_ROOT = BASE_DIR / "uploads/"
+# MEDIA_URL = "media/"
+
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [BASE_DIR / 'static']
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Media files (user uploads)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
